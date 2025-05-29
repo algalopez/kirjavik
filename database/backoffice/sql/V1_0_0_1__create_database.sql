54d20ceec6aaaf6c77a90873f885d79e3f016633
algalopez <algalopez85@gmail.com>
2025-05-29 10:42:34 +0200

    #2: Backoffice user feature
    feat: add db table

--- database/backoffice/sql/V1_0_0_1__create_database.sql
+++ database/backoffice/sql/V1_0_0_1__create_database.sql
@@ -2,12 +2,17 @@ CREATE DATABASE IF NOT EXISTS backoffice;
 
 USE backoffice;
 
-CREATE TABLE IF NOT EXISTS book (
-    id CHAR(36) PRIMARY KEY,
-    isbn VARCHAR(20) NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL,
-    page_count INT,
-    year INT
+CREATE TABLE IF NOT EXISTS `book` (
+    `id` CHAR(36) PRIMARY KEY,
+    `isbn` VARCHAR(20) NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `author` VARCHAR(255) NOT NULL,
+    `page_count` INT,
+    `year` INT
 );
 
+CREATE TABLE IF NOT EXISTS `user` (
+    `id` CHAR(36) PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL UNIQUE
+);
