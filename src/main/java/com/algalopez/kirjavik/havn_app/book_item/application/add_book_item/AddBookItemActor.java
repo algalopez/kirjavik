65a0646bdbcac931b2e262460e1b295cdcf7fb26
alvaro.garcia <alvaro.garcia@sportradar.com>
2025-06-20 13:28:23 +0200

    #4: Pact tests
    Create pact test sample

---
+++ src/main/java/com/algalopez/kirjavik/havn_app/book_item/application/add_book_item/AddBookItemActor.java
@@ -0,0 +1,11 @@
+package com.algalopez.kirjavik.havn_app.book_item.application.add_book_item;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class AddBookItemActor {
+
+  public void command(AddBookItemCommand command) {
+    // Logic not implemented yet
+  }
+}
